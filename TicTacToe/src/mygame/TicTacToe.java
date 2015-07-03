3c1ada5059f6fbf65fe1f5977a376313e63f03a0
stanislav-skrebtsov <akrabato@gmail.com>
2015-07-03 14:17:32 +0300

    Update TicTacToe.java

--- TicTacToe/src/mygame/TicTacToe.java
+++ TicTacToe/src/mygame/TicTacToe.java
@@ -12,7 +12,7 @@ public class TicTacToe extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("tictactoe.fxml"));
         primaryStage.setTitle("TicTacToe");
-        Scene scene = new Scene(root, 300, 275);
+        Scene scene = new Scene(root, 300, 325);
         scene.getStylesheets().add(getClass()
                 .getResource("tictactoe.css").toExternalForm());
         primaryStage.setResizable(false);
